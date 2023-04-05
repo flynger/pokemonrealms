643923b6e0c897a70bad8b7332a5134ceec6963a
GeWangzi <alex_geforce@hotmail.com>
2023-04-05 15:09:02 -0700

    Update battle.ts

---
+++ battle.ts
@@ -0,0 +1,30 @@
+class PokemonBattle {
+
+    partyArray: string[];
+    currentTurn: number;
+
+    constructor(...parties) {
+        this.partyArray = parties;
+        this.currentTurn = 1;
+        console.log(parties)
+    }
+
+    startTurn() {
+        //check pokemon items
+        //check pokemon abilities
+        if (this.partyArray.length > 0) {
+            console.log(`Turn ${this.currentTurn}`);
+
+            //calculate speed
+
+            //make move loop
+
+            //
+        }
+        else {
+
+        }
+    }
+}
+
+new PokemonBattle("hi", "abc", "fig");
