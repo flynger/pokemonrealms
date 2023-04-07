276508c53d27ea6149bcaf0567b2c9044ad99d2a
GeWangzi <56490458+GeWangzi@users.noreply.github.com>
2023-04-07 11:21:15 -0700

    small changes to battle.ts associations added to party.ts

--- src/battle.ts
+++ src/battle.ts
@@ -5,23 +5,27 @@ class PokemonBattle {
 
     partyArray: Party[];
     currentTurn: number;
+    canRun: boolean;
     terrain: string
     weather: string
 
     constructor(...parties: Party[]) {
         this.partyArray = parties;
         this.currentTurn = 1;
-        console.log(parties)
+        console.log(parties);
+        this.partyArray[0].input = [0, 0, 0];
+        this.partyArray[1].input = [0, 0, 0];
     }
 
     startTurn() {
         // check pokemon items
         // check pokemon abilities
+
         if (this.partyArray.length > 0) {
             console.log(`Turn ${this.currentTurn}`);
 
             // calculate speed
-
+            if(this.partyArray[0].pokemon[this.partyArray[0].input[1]].s > this.partyArray[1].pokemon[this.partyArray[1].input[1]])
             // make move loop
 
             //
