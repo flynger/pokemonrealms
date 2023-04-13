0cc1eb42ae0d8a23b12bc679980dce0bf2480304
GeWangzi <alex_geforce@hotmail.com>
2023-04-13 11:44:29 -0700

    Updated pokemonTranslator.ts to also work for abilities and fix evs and ivs

--- src/pokemonTranslator.ts
+++ src/pokemonTranslator.ts
@@ -1,13 +1,41 @@
 import { Pokemon, Ability, AbilitySlot, Gender, Move, Species, Stats, Type } from './pokemon'
 
 export function translatePokemon(pokemon: Pokemon): string {
-    // const ability = `${pokemon.ability}`;
-    const ivs = Object.values(pokemon.ivs).join(",");
-    const evs = Object.values(pokemon.evs).join(",");
-    const statStages = Object.values(pokemon.statStages).join(",");
-    const moves = pokemon.moves.map((move) => move).join(", ");
-  
-    return `${pokemon.species}||${pokemon.item}|${moves}|${pokemon.nature}|${ivs},${evs},${statStages}`;
+    const species = pokemon.species;
+    const item = pokemon.item || "";
+    const ability = pokemon.ability || "";
+    const moves = pokemon.moves.join(",");
+    const nature = pokemon.nature;
+    const evs = [
+      pokemon.evs.hp || "",
+      pokemon.evs.atk || "",
+      pokemon.evs.def || "",
+      pokemon.evs.spa || "",
+      pokemon.evs.spd || "",
+      pokemon.evs.spe || "",
+    ].join(",");
+    const ivs = [
+      pokemon.ivs.hp || "",
+      pokemon.ivs.atk || "",
+      pokemon.ivs.def || "",
+      pokemon.ivs.spa || "",
+      pokemon.ivs.spd || "",
+      pokemon.ivs.spe || "",
+    ].join(",");
+    const level = pokemon.level || "";
+    const shiny = pokemon.shiny ? "S" : "";
+    const gender = pokemon.gender;
+    const statStages = [
+      pokemon.statStages.atk || "",
+      pokemon.statStages.def || "",
+      pokemon.statStages.spa || "",
+      pokemon.statStages.spd || "",
+      pokemon.statStages.spe || "",
+      pokemon.statStages.accuracy || "",
+      pokemon.statStages.crit || "",
+      pokemon.statStages.evasion || "",
+    ].join(",");
+    return `${species}|${item}|${ability}|${moves}|${nature}|${evs}|${ivs}|${level}|${shiny}|${gender}|${statStages}]`;
   }
 
   const froslass: Pokemon = {
