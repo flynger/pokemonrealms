416d699c49fc3af613121eecdd6806e837ea35d1
flynger <arnavsingh2516@gmail.com>
2023-04-18 02:23:17 -0700

    Update battle sim, decent output now

--- src/singleBattle.js
+++ src/singleBattle.js
@@ -1,92 +1,151 @@
-import Text from 'pokemon-showdown/.data-dist/text/default.js';
+import { DefaultText } from 'pokemon-showdown/.data-dist/text/default.js';
+import { MovesText } from 'pokemon-showdown/.data-dist/text/moves.js';
 import Pokemon from './pokemon.js';
 import Party from './party.js';
 import Showdown from 'pokemon-showdown';
+const { BattleStream, Dex } = Showdown;
 
 class SingleBattle {
     constructor(party1, party2) {
-        this.stream = new Showdown.BattleStream();
+        this.stream = new BattleStream();
         (async () => {
             let counter = 0;
             for await (const output of this.stream) {
                 let outputArray = output.split("\n");
-                let previousHpPercentage = 100;
+                let ownActivePokemon = null;
+                let enemyActivePokemon = null;
+                let previousOwnHpPercentage = 100;
+                let previousEnemyHpPercentage = 100;
                 switch (outputArray.shift()) {
                     case "update":
-                        let split = 0; // split counter
+                        let splitCounter = 0; // split counter
                         let splitPlayer; // first line of split, which player to send to
                         for (const line of outputArray) {
                             let lineArray = line.slice(1).split("|");
+                            let args = {}; //args to replace their respective fields in default.ts
+                            let useArgs = true; // whether to replace args or not
+
+                            let isOwnPokemon = lineArray.length > 1 && (lineArray[1].includes("p1") || lineArray[1].includes("p2")) ? lineArray[1].slice(0, 2) == "p1" : null; // whether the message is about your pokemon or the opponent's
                             // if msg is split
-                            if (split > 0) {
-                                if (split === 2) {
-                                    // console.log(`Send msg to ${splitPlayer}: ${lineArray.join(" => ")}`);
-
-                                    switch (lineArray.shift()) {
+                            let denoter = lineArray.shift();
+                            let messageText = DefaultText.default[denoter[0] == "-" ? denoter.substring(1) : denoter] || " ";
+                            if (splitCounter > 0) {
+                                // private information
+                                if (splitCounter == 1) {
+                                    let newPercentage;
+                                    //console.log(`Send msg to ${splitPlayer}: ${lineArray.join(" => ")}`);
+                                    switch (denoter) {
+                                        case "switch":
+                                            if (isOwnPokemon && ownActivePokemon != null) {
+                                                messageText = DefaultText.default.switchOutOwn;
+                                            } else if (!isOwnPokemon && enemyActivePokemon != null) {
+                                                messageText = DefaultText.default.switchOut;
+                                                args.TRAINER = party2.name;
+                                            } else {
+                                                // add logic to send private data to player
+                                                messageText = " ";
+                                            }
+                                            args.NICKNAME = lineArray[0].slice(5);
+                                            break;
                                         case "-damage":
-                                            console.log("Damage");
-                                            let damageArgs = {};
-                                            let damageMessageIndex = 0;
-                                            for (let val of lineArray) {
-                                                console.log(`damageMessageIndex: ${damageMessageIndex}, Value: ${val}, length: ${lineArray.length}`);
-                                                if (damageMessageIndex === 0) {
-                                                    damageArgs.pokemon = val;
-                                                    console.log(`damageArgs.pokemon: ${damageArgs.pokemon}`)
-                                                }
-                                                else if (damageMessageIndex === 1) {
-                                                    let [numerator, denominator] = val.split("/");
-                                                    console.log(`Numerator: ${numerator}, Denominator: ${denominator}`);
-                                                    console.log(`percentage: ${numerator / denominator}`);
-
-                                                    let percentageTaken = previousHpPercentage - (numerator / denominator) * 100;
-                                                    console.log(`${damageArgs.pokemon} lost ${percentageTaken} percent of its health!`);
-                                                    previousHpPercentage = (numerator / denominator) * 100;
-                                                    // console.log(previousHpPercentage);
-                                                }
-                                                damageMessageIndex++;
+                                            messageText = DefaultText.default.damagePercentage;
+                                            args.NICKNAME = lineArray[0].slice(5);
+                                            newPercentage = +lineArray[1].split("/")[0];
+                                            if (isOwnPokemon) {
+                                                args.PERCENTAGE = previousOwnHpPercentage - newPercentage + "%";
+                                                previousOwnHpPercentage = newPercentage;
+                                            } else {
+                                                args.PERCENTAGE = previousEnemyHpPercentage - newPercentage + "%";
+                                                previousEnemyHpPercentage = newPercentage;
+                                            }
+                                            break;
+                                        case "-heal":
+                                            args.NICKNAME = lineArray[0].slice(5);
+                                            newPercentage = +lineArray[1].split("/")[0];
+                                            if (isOwnPokemon) {
+                                                previousOwnHpPercentage = newPercentage;
+                                            } else {
+                                                previousEnemyHpPercentage = newPercentage;
                                             }
                                     }
-                                } else {
-                                    //split == 1
-                                    console.log(`Send public: ${line}`);
+                                    // console.log(`Send private: ${line}`);
                                 }
-                                split--;
+                                // public information
+                                else {
+                                    switch (denoter) {
+                                        case "switch":
+                                            if (isOwnPokemon) {
+                                                messageText = DefaultText.default.switchInOwn;
+                                            } else if (!isOwnPokemon) {
+                                                messageText = DefaultText.default.switchIn;
+                                                args.TRAINER = party2.name;
+                                            }
+                                            args.NICKNAME = lineArray[0].slice(5);
+                                            args.SPECIES = lineArray[1].split(", ")[0];
+                                            // add logic to send public data to player
+                                            break;
+                                        default:
+                                            messageText = " ";
+                                    }
+                                    //splitCounter == 1
+                                    //console.log(`Send public: ${line}`);
+
+                                }
+                                splitCounter--;
                             } else {
-                                let denoter = lineArray.shift();
-                                let args = {}; //args to replace their respective fields in default.ts
-                                let messageText = Text.DefaultText.default[denoter] || "!ERRORWTF!";
-                                let useArgs = true; // whether to replace args or not
                                 switch (denoter) {
-                                    case "split":
-                                        split = 2;
-                                        splitPlayer = lineArray[0];
-                                        continue;
                                     case "move":
                                         args.NICKNAME = lineArray[0].slice(5);
                                         args.MOVE = lineArray[1];
                                         break;
+                                    case "split":
+                                        splitCounter = 2;
+                                        splitPlayer = lineArray[0];
+                                        continue;
+                                    case "start":
+                                        messageText = DefaultText.default.startBattle.replace("[TRAINER]", party1.name).replace("[TRAINER]", party2.name);
+                                        useArgs = false;
+                                        break;
                                     case "turn":
                                         args.NUMBER = lineArray[0];
                                         break;
-                                    case "start":
-                                        messageText = Text.DefaultText.default.startBattle.replace("[TRAINER]", party1.name).replace("[TRAINER]", party2.name);
+                                    case "-miss":
+                                        isOwnPokemon = !isOwnPokemon;
+                                        args.NICKNAME = lineArray[1].slice(5);
+                                        break;
+                                    case "-start":
+                                        let effectDetails = lineArray[1].split(": ");
+                                        let effectSourceType = effectDetails[0];
+                                        let effectSource = effectDetails[1];
+                                        if (effectSourceType == "move") {
+                                            messageText = MovesText[Dex.moves.get(effectSource).id].start;
+                                        }
+                                        else messageText = DefaultText.default.start;
+                                        args.NICKNAME = lineArray[0].slice(5);
+                                        break;
+                                    case "-supereffective":
+                                        messageText = DefaultText.default.superEffective;
                                         useArgs = false;
                                         break;
                                     default:
-                                        continue;
-                                    //console.log(line);
+                                        messageText = " ";
                                 }
-                                if (useArgs) {
-                                    if (messageText.includes("[POKEMON]")) {
-                                        let ownPokemon = lineArray[0].slice(0, 2) == "p1";
-                                        messageText = messageText.replace("[POKEMON]", ownPokemon ? Text.DefaultText.default.pokemon : Text.DefaultText.default.opposingPokemon);
-                                    }
-                                    for (let arg in args) {
-                                        messageText = messageText.replace(`[${arg}]`, args[arg]);
-                                    }
-                                }
-                                // console.log(messageText);
                             }
+                            if (useArgs) {
+                                if (messageText.includes("[POKEMON]")) {
+                                    messageText = messageText.replace("[POKEMON]", isOwnPokemon ? DefaultText.default.pokemon : DefaultText.default.opposingPokemon);
+                                } else if (messageText.includes("[FULLNAME]")) {
+                                    messageText = messageText.replace("[FULLNAME]", "[NICKNAME] (**[SPECIES]**)");
+                                }
+                                for (let arg in args) {
+                                    messageText = messageText.replace(`[${arg}]`, args[arg]);
+                                }
+                            }
+                            let firstLetter = messageText.search(/[a-zA-Z]/);
+                            if (firstLetter != messageText.search(/[A-Z]/)) {
+                                messageText = messageText.substring(0, firstLetter) + messageText[firstLetter].toUpperCase() + messageText.substring(firstLetter + 1);
+                            }
+                            console.log(line + " ".repeat(80 - line.length) + " ===>      " + messageText); // formatting to compare old output to our new, processed output
                         }
                         break;
                     case "sideupdate":
@@ -107,14 +166,12 @@ class SingleBattle {
     }
 
     startBattle() {
-        this.stream.write(`>start {"formatid":"gen7custom"}`);
+        this.stream.write(`>start {"formatid":""}`);
         this.stream.write(`>player p1 ${JSON.stringify(this.playerOptions1)}`);
         this.stream.write(`>player p2 ${JSON.stringify(this.playerOptions2)}`);
         // this.stream.write(`>p1 team 123456`);
         // this.stream.write(`>p2 team 123456`);
     }
-
-
 
     useMove(playerNumber, moveNumber) {
         if (playerNumber == 1) {
@@ -125,7 +182,7 @@ class SingleBattle {
         }
     }
 
-    switch(playerNumber, switchNumber) {
+    switchTo(playerNumber, switchNumber) {
         if (playerNumber == 1) {
             this.stream.write(`>p1 switch ${switchNumber}`);
         }
@@ -196,18 +253,18 @@ const seismitoad = {
 
 const party1 = new Party('Flynger', [
     new Pokemon("bulbasaur", "bulby", "M", undefined, 7, undefined, undefined, "0", undefined, undefined, ["leechseed"]),
-    new Pokemon("articuno", "uno", "N", undefined, 10, undefined, undefined, undefined, undefined, undefined, ["powdersnow"])
+    new Pokemon("articuno", "uno", "N", undefined, 10, undefined, undefined, "0", undefined, undefined, ["powdersnow"])
 ]);
 
 const party2 = new Party('Eichardo', [
     new Pokemon("pidgey", "Bird", "M", undefined, 10, undefined, undefined, undefined, undefined, undefined, ["gust"]),
-    new Pokemon("butterfree", "sad", "M", undefined, 15, undefined, undefined, undefined, undefined, undefined, ["confusion"])
+    new Pokemon("butterfree", "sad", "M", undefined, 15, undefined, undefined, "0", undefined, undefined, ["confusion"])
 ]);
 
 const battle = new SingleBattle(party1, party2);
 
 battle.startBattle();
 battle.useMove(1, 1);
-battle.switch(2, 2);
-battle.switch(1, 2);
+// battle.switchTo(2, 2);
+// battle.switchTo(1, 2);
 battle.useMove(2, 1);
