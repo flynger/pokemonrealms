31127c77339b37ac80bd02249f421428d71c4063
richard-wei23 <60592404+richard-wei23@users.noreply.github.com>
2023-04-25 02:18:06 -0700

    update homepage

---
+++ client/src/home.js
@@ -0,0 +1,20 @@
+/*
+
+
+This file implements the logic for the home page
+*/
+
+$(function () {
+    var x = 0;
+    var y = 0;
+    
+    var hero = $("#hero");
+
+    function moveHero() {
+        hero.css("backgroundPosition", x + 'px' + ' ' + y + 'px');
+        y-=0.2;
+        requestAnimationFrame(moveHero);
+    }
+    
+    moveHero();
+});
