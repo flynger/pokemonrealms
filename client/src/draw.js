ee9d14a93eb980f6de36db50049cd9d593e530ad
GeWangzi <alex_geforce@hotmail.com>
2023-04-28 15:52:19 -0700

    temp

--- client/src/draw.js
+++ client/src/draw.js
@@ -111,62 +111,62 @@ async function setupGame() {
     gameContainer.addChild(graphics);
     app.stage.addChild(gameContainer);
     app.stage.addChild(textContainer);
-    const emitter = new PIXI.particles.Emitter(PIXI.Assets.particle, {
-        "alpha": {
-            "start": 0.8,
-            "end": 0.1
-        },
-        "scale": {
-            "start": 0.5,
-            "end": 0.1,
-            "minimumScaleMultiplier": 1
-        },
-        "color": {
-            "start": "#ffffff",
-            "end": "#ffffff"
-        },
-        "speed": {
-            "start": 200,
-            "end": 200,
-            "minimumSpeedMultiplier": 1
-        },
-        "acceleration": {
-            "x": 0,
-            "y": 0
-        },
-        "maxSpeed": 0,
-        "startRotation": {
-            "min": 0,
-            "max": 360
-        },
-        "noRotation": false,
-        "rotationSpeed": {
-            "min": 0,
-            "max": 0
-        },
-        "lifetime": {
-            "min": 0.1,
-            "max": 0.75
-        },
-        "blendMode": "normal",
-        "frequency": 0.005,
-        "emitterLifetime": -1,
-        "maxParticles": 1000,
-        "pos": {
-            "x": 0,
-            "y": 0
-        },
-        "addAtBack": false,
-        "spawnType": "circle",
-        "spawnCircle": {
-            "x": 0,
-            "y": 0,
-            "r": 10
-        }
-    });
+    // const emitter = new PIXI.particles.Emitter(PIXI.Assets.particle, {
+    //     "alpha": {
+    //         "start": 0.8,
+    //         "end": 0.1
+    //     },
+    //     "scale": {
+    //         "start": 0.5,
+    //         "end": 0.1,
+    //         "minimumScaleMultiplier": 1
+    //     },
+    //     "color": {
+    //         "start": "#ffffff",
+    //         "end": "#ffffff"
+    //     },
+    //     "speed": {
+    //         "start": 200,
+    //         "end": 200,
+    //         "minimumSpeedMultiplier": 1
+    //     },
+    //     "acceleration": {
+    //         "x": 0,
+    //         "y": 0
+    //     },
+    //     "maxSpeed": 0,
+    //     "startRotation": {
+    //         "min": 0,
+    //         "max": 360
+    //     },
+    //     "noRotation": false,
+    //     "rotationSpeed": {
+    //         "min": 0,
+    //         "max": 0
+    //     },
+    //     "lifetime": {
+    //         "min": 0.1,
+    //         "max": 0.75
+    //     },
+    //     "blendMode": "normal",
+    //     "frequency": 0.005,
+    //     "emitterLifetime": -1,
+    //     "maxParticles": 1000,
+    //     "pos": {
+    //         "x": 0,
+    //         "y": 0
+    //     },
+    //     "addAtBack": false,
+    //     "spawnType": "circle",
+    //     "spawnCircle": {
+    //         "x": 0,
+    //         "y": 0,
+    //         "r": 10
+    //     }
+    // });
 
-    // Start emitting particles
-    emitter.emit = true;
+    // // Start emitting particles
+    // emitter.emit = true;
 }
 
 function draw(deltaTime) {
