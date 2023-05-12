82853f2804f5bfdc3f304b7dc07b20a8f79c4a61
GeWangzi <alex_geforce@hotmail.com>
2023-05-11 22:57:29 -0700

    confus

---
+++ client/src/npc.js
@@ -0,0 +1,13 @@
+import Player from "../../src/player";
+class npc {
+    static npcs = {};
+    constructor(x, y) {
+        this.npcSprite = new PIXI.Sprite(kyledoveSheet.textures['log1']);
+        this.npcSprite.zIndex = this.rightSprite.zIndex = y - 19;
+        this.npcSprite.x = x;
+        this.npcSprite.y = y;
+        gameContainer.addChild(this.npcSprite);
+        colliders[[x, y]] = new collider(x, y, 32, 32);
+        //colliders[[x + 32, y]] = new collider(x + 32, y, 32, 32);
+    }
+}
