4cfbb7a48a80c25554fe5e39b45cba77a675ab30
flynger <36573101+flynger@users.noreply.github.com>
2023-05-17 17:08:35 -0700

    fixed harry code

--- src/pokemart.js
+++ src/pokemart.js
@@ -1,16 +1,21 @@
-import { ItemsText } from 'pokemon-showdown/.data-dist/text/items.js';
+import { ItemsText } from 'pokemon-showdown/.data-dist/text/items.js'; // held items only
 export default class Pokemart {
     constructor(items) {
         this.catalog = items;
-        for(let item in catalog) {
-            if(ItemsText[item.id]){
-                item.name = ItemsText[item.id.name];
-                item.desc = ItemsText[item.id.desc];
+        for (let item of this.catalog) {
+            if (ItemsText[item.id]) {
+                item.name = ItemsText[item.id].name;
+                item.desc = ItemsText[item.id].desc;
             }
         }
     }
-    
-
 }
 
-new Pokemart([{id: "masterball", price: 1}, {id: "thunderstone", price: 50}, {id: "aloraichiumz", price: 200}, {id: "aguavberry", price: 5}, {id: "antidote", price: 10}]);
+var testmart = new Pokemart([
+    { id: "pokeball", price: 200 },
+    { id: "greatball", price: 650 },
+    { id: "aloraichiumz", price: 200 },
+    { id: "aguavberry", price: 5 },
+    { id: "thunderstone", price: 10000 }
+]);
+console.log(testmart);
