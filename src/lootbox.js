bdc7a03c7635d7ffeeb738eb6e16f9486de2d56b
Harry <harryliu0524@gmail.com>
2023-05-31 14:41:42 -0700

    lootbox

--- src/lootbox.js
+++ src/lootbox.js
@@ -113,6 +113,68 @@ var loot = {
         }
     ]
 }
+var lootbox = {
+    common: {
+        chances: {
+            common: 0.7,
+            uncommon: 0.2039,
+            rare: 0.08,
+            epic: 0.015,
+            legendary: 0.001,
+            mythic: 0.0001
+        }
+    },
+    uncommon: {
+        chances: {
+            common: 0.3,
+            uncommon: 0.5,
+            rare: 0.1805,
+            epic: 0.015,
+            legendary: 0.004,
+            mythic: 0.0005
+        }
+    },
+    rare: {
+        chances: {
+            common: 0.2,
+            uncommon: 0.35,
+            rare: 0.38,
+            epic: 0.065,
+            legendary: 0.004,
+            mythic: 0.001
+        }
+    },
+    epic: {
+        chances: {
+            common: 0.1,
+            uncommon: 0.2,
+            rare: 0.4,
+            epic: 0.2,
+            legendary: 0.075,
+            mythic: 0.025
+        }
+    },
+    legendary: {
+        chances: {
+            common: 0.1,
+            uncommon: 0.1,
+            rare: 0.15,
+            epic: 0.45,
+            legendary: 0.14,
+            mythic: 0.06
+        }
+    },
+    mythic: {
+        chances: {
+            common: 0.0,
+            uncommon: 0.0,
+            rare: 0.05,
+            epic: 0.5,
+            legendary: 0.3,
+            mythic: 0.15
+        }
+    }
+}
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
