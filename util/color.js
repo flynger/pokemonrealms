39d35deb440dae312a2deea805c6e8fa39e4621a
flynger <36573101+flynger@users.noreply.github.com>
2024-01-10 14:44:47 -0800

    moved stuff around

---
+++ util/color.js
@@ -0,0 +1,8 @@
+export default class color {
+  static blue = '\x1b[36m%s\x1b[0m';
+  static white = '\x1b[0m%s\x1b[0m';
+  static red = '\x1b[31m%s\x1b[0m';
+  static green = '\x1b[32m%s\x1b[0m';
+  static yellow = '\x1b[33m%s\x1b[0m';
+  static pink = '\x1b[35m%s\x1b[0m';
+};
