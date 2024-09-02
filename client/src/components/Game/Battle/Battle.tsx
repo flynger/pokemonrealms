2a3982b8f4526e46ef52b1777efcb53e3b5fce6a
Arnav Singh <36573101+flynger@users.noreply.github.com>
2024-09-01 20:27:25 -0700

    battle dialogue

--- client/src/components/Game/Battle/Battle.tsx
+++ client/src/components/Game/Battle/Battle.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import "./Battle.css";
 import HpBar from "./components/HpBar";
 import MonLabel from "./components/MonLabel";
 import XpBar from "./components/XpBar";
+
+type MessageState = "actions" | "fight" | "message";
+
 export default function Battle() {
     const you = {
         name: "Mareep",
@@ -17,7 +21,8 @@ export default function Battle() {
         level: 3,
         shiny: false,
         hpPercent: 26
-    }
+    };
+    const [messageState, setMessageState] = useState<MessageState>("message");
     return (
         <div id="battle">
             <div id="battle-container">
@@ -51,18 +56,32 @@ export default function Battle() {
                         </div>
                     </div>
                 </div>
-                {/* <div className="message" id="battle-msg">Go! Squirtle!</div> */}
-                <div className="message" id="battle-btns">
-                    <div id="battle-btns-message">
-                        What will {you.name} do?
-                    </div>
-                    <div id="battle-btns-container">
-                        <button className="battle-btn fight">FIGHT</button>
-                        <button className="battle-btn bag">BAG</button>
-                        <button className="battle-btn switch">SWITCH</button>
-                        <button className="battle-btn run">RUN</button>
-                    </div>
+                <div className="message">
+                    <DialogueBox text="But our sides can have multiple people so maybe we should put active on every party?" />
                 </div>
+                {/* {messageState === "message" && <div className="message">
+                    <span id="battle-msg">Go! Squirtle!</span>
+                </div>}
+                {messageState !== "message" && <div className="message" id="battle-btns">
+                    {messageState === "actions" &&
+                        <>
+                            <div id="battle-btns-message">
+                                What will {you.name} do?
+                            </div>
+                            <div id="battle-btns-container">
+                                <button className="battle-btn fight" onClick={() => setMessageState("fight")}>FIGHT</button>
+                                <button className="battle-btn bag">BAG</button>
+                                <button className="battle-btn switch">SWITCH</button>
+                                <button className="battle-btn run">RUN</button>
+                            </div>
+                        </>}
+                    {messageState === "fight" && <div id="fights-btns-container">
+                        <button className="battle-btn"></button>
+                        <button className="battle-btn"></button>
+                        <button className="battle-btn"></button>
+                        <button className="battle-btn"></button>
+                    </div>}
+                </div>} */}
                 {/* <div id="battle-btns">
                 </div>
                 <div id="battle-btns-footer">
@@ -71,3 +90,16 @@ export default function Battle() {
         </div>
     );
 }
+
+type DialogueBoxProps = { text: string };
+const DialogueBox = ({ text }: DialogueBoxProps) => {
+    return (
+      <div id="battle-dialogue">
+        {text.split('').map((char, index) => (
+          <span key={index} style={{ animationDelay: `${index * 0.005}s` }}>
+            {char}
+          </span>
+        ))}
+      </div>
+    );
+  };
